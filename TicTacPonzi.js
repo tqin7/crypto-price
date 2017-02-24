d1e62f9f5b370090cf6a4add4a0beba6fdff1a3f
Tian Qin <Tian Qin>
2017-02-24 12:04:14 -0800

    Added functionality. Not complete yet

--- TicTacPonzi.js
+++ TicTacPonzi.js
@@ -26,11 +26,11 @@ contract TicTacPonzi {
   address public creator;
   //Minimum amount of ether required to participate
   uint threshold;
-  //Token of winner
-  uint8 winner;
 
   //Console logs event when move is made
-  event madeMove(address player, uint8 move);
+  event madeMove(uint8 token, uint8 row, uint8 col);
+
+  event Won(uint8 token);
 
   //A complex type that represents a player
   struct Player {
@@ -49,8 +49,16 @@ contract TicTacPonzi {
   when the game ends.*/
   mapping (address=>uint) underdogs;
 
+  modifier hasValue {
+    if (msg.value > 0) {
+      _;
+    } else {
+      throw;
+    }
+  }
+
   /** Setting Up Game*/
-  function TicTacPonzi(uint _threshold) payable {
+  function TicTacPonzi(uint _threshold) payable hasValue{
     if (_threshold > msg.value) {
       throw;
     }
@@ -64,7 +72,7 @@ contract TicTacPonzi {
     });
   }
 
-  function challengerJoin() payable {
+  function challengerJoin() public payable hasValue{
     if (msg.value < threshold) {
       underdogs[msg.sender] = msg.value;
     } else {
@@ -77,12 +85,53 @@ contract TicTacPonzi {
     }
   }
 
+  function checkThreshold() public {
+    return threshold;
+  }
+
   /** Game Part */
   //Virtual representation of the game board
   uint8[3][3] board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  //Determine player's turn: 1 is payee, 2 is challenger
+  uint8 turn = 1;
+  //Token of winner
+  uint8 winner = 0;
 
-  //Check
-  function checkBoard() returns (uint8){
+  function makeMove(uint8 _row, uint _col) public {
+    if (players[msg.sender].isActive == true && turn == players[msg.sender].token && board[_row - 1][_col - 1] == 0) {
+      board[_row - 1][_col - 1] = players[msg.sender].token;
+      event(players[msg.sender].token, _row, _col);
+      checkWinner();
+      changeTurn();
+    } else {
+      throw;
+    }
+  }
+
+  function checkWinner() private {
+    winner = checkBoard();
+    if (winner != 0) {
+
+      Won(winner);
+    }
+  }
+
+  /** Resets game depending on who wins*/
+  function reset() private{
+    
+  }
+
+  /** Modify uint8 turn */
+  function changeTurn() private {
+    if (turn == 1) {
+      turn = 2;
+    } else {
+      turn = 1;
+    }
+  }
+
+  //Check all possible ways of winning and return 0, 1, 2 depending on situations
+  function checkBoard() private returns (uint8) {
     checkRows();
     checkColumns();
     checkDiags();
@@ -90,7 +139,7 @@ contract TicTacPonzi {
   }
 
   /** Check rows and return winner's token if any, otherwise 0*/
-  function checkRows() {
+  function checkRows() private {
     for (uint i = 0; i < board.length; i++) {
       if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
         winner = board[i][0];
@@ -99,7 +148,7 @@ contract TicTacPonzi {
   }
 
   /** Check columns and return winner's token if any, otherwise 0*/
-  function checkColumns() {
+  function checkColumns() private {
     for (uint i = 0; i < board[0].length; i++) {
       if (board[0][i] != 0 && board[0][i] == board[1][i] &&  board[0][i] == board[2][i]) {
         winner = board[0][i];
@@ -108,7 +157,7 @@ contract TicTacPonzi {
   }
 
   /** Check diagonals and return winner's token if any, otherwise 0*/
-  function checkDiags() {
+  function checkDiags() private {
     if (board[1][1] != 0 && board[1][1] == board[0][0] && board[1][1] == board[2][2] || board[1][1] != 0 && board[1][1] == board[0][2] && board[1][1] == board[2][0]) {
       winner = board[1][1];
     }
